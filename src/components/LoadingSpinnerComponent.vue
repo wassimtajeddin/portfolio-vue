d7fc6e903df4b645a3b5f1f809e4879360270275
wassimtajeddin <wassim.tajeddin@gmail.com>
2026-01-03 13:54:56 +0100

    Add loading spinner component

---
+++ src/components/LoadingSpinnerComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-if="isLoading" class="loading-overlay">
+    <div class="spinner">
+      <div class="dot1"></div>
+      <div class="dot2"></div>
+      <div class="dot3"></div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const isLoading = ref(true)
+
+onMounted(() => {
+  setTimeout(() => {
+    isLoading.value = false
+  }, 1000)
+})
+</script>
+
+<style scoped>
+</style>
